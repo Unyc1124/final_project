5c7b568e8260325636264edc9f67970def691c4b
Unyc1124 <aditidubey792@gmail.com>
2024-10-07 23:55:53 +0530

    1st

---
+++ lib/screens/chat.dart
@@ -0,0 +1,29 @@
+// import 'package:final_project/widgets/chat_messages.dart';
+// import 'package:final_project/widgets/new_message.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        
+        title: const Text('T A L K Y'), 
+        actions: [
+        IconButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            icon: Icon(
+              Icons.exit_to_app,
+              color: Theme.of(context).colorScheme.primary,
+            )),
+      ]),
+    body: const Center(
+      child: Text('logged in !'),
+    ),
+    );
+  }
+}
